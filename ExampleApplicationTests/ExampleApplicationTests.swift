28571ab3227705c608073d10e934ca02bd51c6f2
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-01-11 12:54:35 -0800

    Initial Setup (#1)
    # Initial Setup
    
    ## :gear: Release Notes 
    - Runs the Stanford Spezi Template Application setup script
    
    
    ### Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ ExampleApplicationTests/ExampleApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the Example Application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import ExampleApplication
+import XCTest
+
+
+class ExampleApplicationTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
