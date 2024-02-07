97fa87ee289f725244b016c64ef8cc384503c610
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-02-06 16:19:48 -0800

    Merge Conflict

---
+++ ExampleApplication/AppleWatchIsCool.swift
@@ -0,0 +1,21 @@
+//
+// This source file is part of the Example Application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+
+struct AppleWatchIsCool: View {
+    var body: some View {
+        Image(systemName: "applewatch")
+    }
+}
+
+
+#Preview {
+    AppleWatchIsCool()
+}
